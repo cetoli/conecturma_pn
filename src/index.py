eb317395dcb7bfd34b53100d264906223eaca70f
Carlo E. T. Oliveira <carlo@nce.ufrj.br>
2018-06-15 12:15:33 -0300

    Merge remote-tracking branch 'remotes/conecturma_origin/master' into devCrudAdmin
    # Conflicts:
    #	src/control/login_controller.py
    #	src/index.py

--- src/index.py
+++ src/index.py
@@ -1,24 +1,5 @@
 from bottle import default_app
-from control.static_controller import *
-from control.menu_controller import *
-from control.usuarios.usuario_controller import *
-from control.usuarios.aluno_controller import *
-from control.usuarios.observador_controller import *
-from control.usuarios.diretor_controller import *
-from control.usuarios.professor_controller import *
-from control.usuarios.gestor_controller import *
-from control.usuarios.administrador_controller import *
-from control.jogo_controller import *
-from control.instituicoes.turma_controller import *
-from control.loja_controller import *
-from control.instituicoes.escola_controller import *
-from control.instituicoes.rede_controller import *
-from control.historico_controller import *
 from control.login_controller import *
-# from control.login.login_controller import *
-# from control.login.login_observador_controller import *
-# from control.login.login_aluno_controller import *
-from control.medalha_controller import *
 
 import bottle
 import os
@@ -26,8 +7,13 @@ import os
 view_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'view')
 bottle.TEMPLATE_PATH.insert(0, view_path)
 
-
 application = default_app()
 
+
+@route('/error403')
+def error403():
+    return template('error403.tpl')
+
+
 if __name__ == '__main__':
-    run(host='localhost', port=8080, reloader=False ,debug=True)
+    bottle.run(host='localhost', port=8080, reloader=False, debug=True)
